a667b27dfba07fb04e354b172d527d9659bf1f53
Noam-Elron <noam.elron.college@gmail.com>
2023-03-05 15:33:04 +0200

    better control flow implementation

--- chatbot.py
+++ chatbot.py
@@ -1,68 +1,51 @@
-from flask import Flask, request, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 from openai_init import generate_prompt, generate_image
-from database import Database
-from utils import get_number_details
+from configparser import ConfigParser
+from client import Client
 
+config = ConfigParser()
+config.read('config.ini')
 
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def link():
-    return render_template("templates/whatsapp_page.html")
-
-@app.route('/whatsapp', methods=['POST'])
-def main():
+def chatbot(request):
+    # The request key: "From" contains the extended whatsapp number of the current user
+    extended_number = request.form.get("From")
+    # The request key: "Body" contains the message the user sent
     incoming_msg = request.values.get('Body', '').lower()
-    phone_number_extended = request.form.get("From")
+    resp = main_process(extended_number, incoming_msg)
+    return str(resp)
 
+
+def main_process(user_phone_details, incoming_msg):
     resp = MessagingResponse()
-    msg = resp.message()
-    responded = False
+    response_msg = resp.message()
+
+    user = Client(user_phone_details)
     
-    user_id, phone_number = get_number_details(phone_number_extended)
-    db = Database()
-    user_exists: bool = db.check_user(user_id)
-    if user_exists is None:
-        db.create_user(user_id, phone_number)
-        msg.body("""Hello, I've detected that you're a new user. Before we begin let me give you some details on how to use me properly.
-                 \nTo create a text generation response include any of these keywords: text, story or default.
-                 \nTo create an image generation response include any of these keywords: image, draw or picture.
-                 \nIf you ever forget how to use me, just type "help" in chat and you'll receive instructions :).
-                 \nNow that you know how what to do, if your previous message was a prompt please repeat it so it can be processed, if not go ahead and create your first response now!""")
-        responded = True
-        return str(resp)
-
+    # If this is a new user then dont do anything except send the user instructions. No need to save data to database other than creating the user and thats implemented in the login method of the Client class.
+    if not user.login():
+        response_msg.body(config.get("automated_responses", "new_user")) 
+        return resp
+    
+    # If a user's current message is either "Text" or "Image", then dont bother checking last message
+    if incoming_msg == "text" or incoming_msg == "image":
+        response = f"Input received, prompt type selected: {incoming_msg}. You have one hour to enter your prompt before the session expires"
     else:
-        image_resp = False
-        text_keywords = ['text', 'story', 'default']
-        image_keywords = ['image', 'draw', 'picture']
-        chatbot_text = True if len(list(filter(lambda word: True if word in text_keywords else False, incoming_msg.split()))) >= 1 else False
-        chatbot_img = True if len(list(filter(lambda word: True if word in image_keywords else False, incoming_msg.split()))) >= 1 else False
-        if chatbot_text:
-            response = generate_prompt(incoming_msg, 50)
-            responded = True
-        elif chatbot_img:
-            response = generate_image(incoming_msg, 1)
-            msg.media(response)
-            image_resp = True
-            responded = True
-        elif 'help' in incoming_msg:
-            response = 'To create a text generation response include any of these keywords: text, story or default. \n To create an image generation response include any of these keywords: image, draw or picture.'
-        if not responded:
-            response = 'Try creating a prompt that includes these keywords \n Text creating: "text", "story", "default". \n Image creation: "image", "draw", "picture".'
-        
-        if image_resp != True:
-            msg.body(response)
+        last_message = user.check_last_message()
+        img = False
+        if last_message == "text":
+            response = generate_prompt(incoming_msg, 150)
+        elif last_message == "image":
+            img = True
+            response = generate_image(incoming_msg)
+            response_msg.media(response)
+        else:
+            response = config.get("automated_responses", "help")
 
-        db.insert_message(user_id, incoming_msg)
-        db.insert_response(response)
-        db.close()
-        return str(resp)
+    if not img:
+        response_msg.body(response)
+    
+    user.insert_message(incoming_msg)
+    user.insert_response(response)
+    return resp
 
 
-
-
-
-if __name__ == '__main__':
-    app.run(port=4000)
